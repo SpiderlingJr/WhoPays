ac71fdbb47509bf28ee6c11b9ef5a1c63fc211f1
Chris Brinker <christopherbrinker@web.de>
2021-12-05 20:01:48 +0100

    Initial commit

---
+++ app/src/main/java/scom/example/whopays/MainActivity.kt
@@ -0,0 +1,11 @@
+package scom.example.whopays
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
